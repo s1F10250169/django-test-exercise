67e9c1178c87cffac7a6ec5040c65bc5cc0dba20
1F10230236 <s1f102302360@iniad.org>
2026-07-09 11:33:50 +0900

    Add-edit-test

--- todo/tests.py
+++ todo/tests.py
@@ -110,3 +110,29 @@ class TodoViewTestCase(TestCase):
         response = client.get('/1/')
 
         self.assertEqual(response.status_code, 404)
+
+    def test_update_get_success(self):
+        task = Task(title='task1', due_at=timezone.make_aware(datetime(2024, 7, 1, 0, 0, 0)))
+        task.save()
+        client = Client()
+        response = client.get('/{}/update/'.format(task.pk))
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.templates[0].name, 'todo/edit.html')
+        self.assertEqual(response.context['task'], task)
+
+    def test_update_post_success(self):
+        due = timezone.make_aware(datetime(2024, 7, 1, 0, 0, 0))
+        task = Task(title='task1', due_at=due)
+        task.save()
+        client = Client()
+        data = {'title': 'updated task', 'due_at': '2024-08-01 12:00:00'}
+
+        response = client.post('/{}/update/'.format(task.pk), data)
+
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response.url, '/{}/'.format(task.pk))
+
+        task.refresh_from_db()
+        self.assertEqual(task.title, 'updated task')
+        self.assertEqual(task.due_at, timezone.make_aware(datetime(2024, 8, 1, 12, 0, 0)))
